3a39f30e55cad5145cca4f1c7ca97a9f0ce3bac0
zhengow <313407040@qq.com>
2022-12-09 00:23:32 +0800

    feat: get sth

---
+++ backtesting_engine/account.go
@@ -0,0 +1,89 @@
+package backtesting_engine
+
+import (
+    "github.com/zhengow/vngo/consts"
+    "github.com/zhengow/vngo/model"
+    "github.com/zhengow/vngo/utils"
+)
+
+type accountEngine struct {
+    priceTicks        map[string]int
+    cash              float64
+    activeLimitOrders map[int]*model.Order
+    limitOrderCount   int
+    closes            map[model.Symbol]float64
+    positions         map[model.Symbol]float64
+    //*positionEngine
+}
+
+func (o *accountEngine) Buy(symbol model.Symbol, price, volume float64) int {
+    return o.sendOrder(symbol, consts.DirectionEnum.LONG, price, volume)
+}
+
+func (o *accountEngine) Sell(symbol model.Symbol, price, volume float64) int {
+    return o.sendOrder(symbol, consts.DirectionEnum.SHORT, price, volume)
+}
+
+func (o *accountEngine) sendOrder(symbol model.Symbol, direction consts.Direction, price, volume float64) int {
+    priceTick := 5
+    if val, ok := o.priceTicks[symbol.Symbol]; ok {
+        priceTick = val
+    }
+    price = utils.RoundTo(price, priceTick)
+    o.limitOrderCount++
+    order := model.NewOrder(symbol, o.limitOrderCount, direction, price, volume)
+    o.activeLimitOrders[o.limitOrderCount] = order
+    return o.limitOrderCount
+}
+
+func (o *accountEngine) CancelAll() {
+    o.activeLimitOrders = make(map[int]*model.Order)
+}
+
+func (o *accountEngine) startTrading() {
+    //o.trading = true
+}
+
+func (o *accountEngine) GetPositions() map[model.Symbol]float64 {
+    return o.positions
+}
+
+func (o *accountEngine) updatePositions(symbol model.Symbol, incrementPos, price float64) {
+    o.positions[symbol] += incrementPos
+    o.cash -= incrementPos * price
+}
+
+func (o *accountEngine) setPriceTicks(priceTicks map[string]int) {
+    o.priceTicks = priceTicks
+}
+
+func (o *accountEngine) GetCash() float64 {
+    return o.cash
+}
+
+func (o *accountEngine) AddCash(increment float64) {
+    o.cash += increment
+}
+
+func (o *accountEngine) GetBalance() float64 {
+    balance := o.cash
+    for symbol, position := range o.positions {
+        closePrice := o.closes[symbol]
+        balance += closePrice * position
+    }
+    return balance
+}
+
+func (o *accountEngine) updateCloses(bars map[string]model.Bar) {
+    for _, bar := range bars {
+        o.closes[bar.Symbol] = bar.ClosePrice
+    }
+}
+
+func newOrderEngine() *accountEngine {
+    return &accountEngine{
+        activeLimitOrders: make(map[int]*model.Order),
+        positions:         make(map[model.Symbol]float64),
+        closes:            make(map[model.Symbol]float64),
+    }
+}
--- strategy/base_strategy.go
+++ strategy/base_strategy.go
@@ -1,42 +1,45 @@
 package strategy
 
 import (
-	"fmt"
-	"reflect"
-	"time"
-
-	"github.com/zhengow/vngo/model"
+    "fmt"
+    "github.com/zhengow/vngo/model"
+    "reflect"
+    "time"
 )
 
 type accountInterface interface {
-	Buy(symbol model.Symbol, price, volume float64) int
-	Sell(symbol model.Symbol, price, volume float64) int
-	CancelAll()
-	GetPositions() map[model.Symbol]float64
-	CurrentTime() time.Time
+    Buy(symbol model.Symbol, price, volume float64) int
+    Sell(symbol model.Symbol, price, volume float64) int
+    CancelAll()
+    GetPositions() map[model.Symbol]float64
     GetCash() float64
     GetBalance() float64
 }
 
-type BaseStrategy struct {
-	accountInterface
-	activeOrderIds []int
+type marketInterface interface {
+    CurrentTime() time.Time
 }
 
-func (s *BaseStrategy) Inject(sI accountInterface) {
-	s.accountInterface = sI
+type BaseStrategy struct {
+    accountInterface
+    marketInterface
+    activeOrderIds []int
+}
+
+func (s *BaseStrategy) Inject(aI accountInterface) {
+    s.accountInterface = aI
 }
 
 func (s *BaseStrategy) SetSetting(strategy interface{}, setting map[string]interface{}) {
-	fields := reflect.ValueOf(strategy).Elem()
-	for name, value := range setting {
-		filedValue := reflect.ValueOf(value)
-		if field := fields.FieldByName(name); field.CanSet() {
-			field.Set(filedValue)
-		} else {
-			fmt.Printf("%s can't set\n", name)
-		}
-	}
+    fields := reflect.ValueOf(strategy).Elem()
+    for name, value := range setting {
+        filedValue := reflect.ValueOf(value)
+        if field := fields.FieldByName(name); field.CanSet() {
+            field.Set(filedValue)
+        } else {
+            fmt.Printf("%s can't set\n", name)
+        }
+    }
 }
 
 func (s *BaseStrategy) OnBars(bars map[string]model.Bar) {
@@ -46,8 +49,8 @@ func (s *BaseStrategy) UpdateTrade(trade model.TradeData) {
 }
 
 type Strategy interface {
-	Inject(accountInterface)
-	SetSetting(interface{}, map[string]interface{})
-	OnBars(map[string]model.Bar)
-	UpdateTrade(model.TradeData)
+    Inject(accountInterface)
+    SetSetting(interface{}, map[string]interface{})
+    OnBars(map[string]model.Bar)
+    UpdateTrade(model.TradeData)
 }
